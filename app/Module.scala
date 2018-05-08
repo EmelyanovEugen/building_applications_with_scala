bd658b413e1e8c83ed8b6b25f90973d4d3931924
EmelyanovEugen <EmelyanovEugen@hotmail.com>
2018-05-08 16:26:35 +0700

    part of the changes to work with Slick

--- app/Module.scala
+++ app/Module.scala
@@ -1,12 +1,23 @@
-import com.google.inject.AbstractModule
 import java.time.Clock
-import services.{ApplicationTimer}
+import com.google.inject.AbstractModule
+import javax.inject.Singleton
+import services.ApplicationTimer
+import services.IImageService
+import services.IPriceSerice
 import services.IProductService
+import services.IReviewService
+import services.IRndService
+import services.ImageService
+import services.PriceService
 import services.ProductService
 import services.ReviewService
-import services.IReviewService
-import services.ImageService
-import services.IImageService
+import services.RndService
+import dao.IProductDao
+import dao.ProductDao
+import dao.ImageDao
+import dao.IImageDao
+import dao.ReviewDao
+import dao.IReviewDao
 
 /**
  * This class is a Guice module that tells Guice how to bind several
@@ -30,6 +41,13 @@ class Module extends AbstractModule {
     bind(classOf[IProductService]).to(classOf[ProductService]).asEagerSingleton()
     bind(classOf[IReviewService]).to(classOf[ReviewService]).asEagerSingleton()
     bind(classOf[IImageService]).to(classOf[ImageService]).asEagerSingleton()
+    
+    bind(classOf[IPriceSerice]).to(classOf[PriceService]).asEagerSingleton()
+    bind(classOf[IRndService]).to(classOf[RndService]).asEagerSingleton()
+    
+    bind(classOf[IProductDao]).to(classOf[ProductDao]).asEagerSingleton()
+    bind(classOf[IImageDao]).to(classOf[ImageDao]).asEagerSingleton()
+    bind(classOf[IReviewDao]).to(classOf[ReviewDao]).asEagerSingleton()
     
   }
 
--- build.sbt
+++ build.sbt
@@ -10,7 +10,12 @@ libraryDependencies ++= Seq(
   jdbc,
   cache,
   ws,
-  "org.scalatestplus.play" %% "scalatestplus-play" % "1.5.1" % Test
+  "org.scalatestplus.play" %% "scalatestplus-play" % "1.5.1" % Test,
+  "com.netflix.rxjava" % "rxjava-scala" % "0.20.7",
+  "com.typesafe.play" %% "play-slick" % "2.0.0",
+  "com.typesafe.play" %% "play-slick-evolutions" % "2.0.0",
+  "postgresql" % "postgresql" % "9.1-901.jdbc4"                         //"mysql" % "mysql-connector-java" % "6.0.3"
 )
 
 resolvers += "scalaz-bintray" at "http://dl.bintray.com/scalaz/releases"
+resolvers += DefaultMavenRepository
